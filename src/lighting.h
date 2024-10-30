f51d05e3b5c495755885c892b9c7e7938612ca62
Taylor Winning <taylor.winning@fundamentals-group.com>
2024-10-30 21:38:59 +0800

    Hopefully more stable

--- src/lighting.h
+++ src/lighting.h
@@ -5,10 +5,9 @@
 
 class Lighting {
 public:
-    static const int NUM_LEDS = 216;
+    static const int NUM_LEDS = 237;
     CRGB leds1[NUM_LEDS];
     CRGB leds2[NUM_LEDS];
-    CRGB leds3[NUM_LEDS];
 
     CRGBPalette16 fairy_palette = CRGBPalette16(
         CHSV(128, 255, 255),  // Aqua (hue 128)
@@ -30,7 +29,6 @@ public:
     Lighting() {
         FastLED.addLeds<WS2812B, 15, GRB>(leds1, NUM_LEDS);
         FastLED.addLeds<WS2812B, 27, GRB>(leds2, NUM_LEDS);
-        FastLED.addLeds<WS2812B, 32, GRB>(leds3, NUM_LEDS);
         FastLED.setBrightness(255);
     }
 
@@ -38,72 +36,25 @@ public:
         int current_brightness = FastLED.getBrightness();
         int new_brightness = constrain(current_brightness + amount, 0, 255);
         FastLED.setBrightness(new_brightness);
-        FastLED.show();
     }
 
     void clear_strip(CRGB* leds) {
         fill_solid(leds, NUM_LEDS, CRGB::Black);
-        FastLED.show();
     }
 
-    void create_sparkle(CRGB* leds, uint8_t start_index, uint8_t sparkle_size, CRGB sparkle_colour) {
-        uint8_t center = start_index + (sparkle_size / 2);
-        uint8_t max_distance = (sparkle_size) / 2;
-
-        for (uint8_t offset = 0; offset < sparkle_size; offset++) {
-            uint8_t ledIndex = start_index + offset;
-            uint8_t distance = abs(int(ledIndex) - int(center));
-            uint8_t brightness = 255 - ((distance * 255) / max_distance);
-            leds[ledIndex] = sparkle_colour;
-            leds[ledIndex].nscale8(brightness);
-        }
-    }
-
-    void petting_zoo_room(CRGB* leds) {
-        fadeToBlackBy(leds, NUM_LEDS, 1);
-        FastLED.show();
-
-        int num_sparkles = random8(8, 20);
-        for (uint8_t i = 0; i < num_sparkles; i++) {
-            uint8_t sparkle_size = random8(10, 25);
-            uint8_t start_index = random8(NUM_LEDS - sparkle_size);
-            CRGB sparkle_colour = ColorFromPalette(petting_zoo_palette, random8(), 255, NOBLEND);
-            create_sparkle(leds, start_index, sparkle_size, sparkle_colour);
-        }
-
-        FastLED.show();
-    }
-
-    void ghoul_room(CRGB* leds) {
-        int index = 0;
-        for (int r = 0; r <= 255; r += 10) {
-            for (int g = 0; g <= 255; g += 10) {
-                CRGB color = CRGB(r, g, 255);
-                leds[index] = color;
-                index++;
-                if (index >= NUM_LEDS) {
-                    FastLED.show();
-                    delay(1000);
-                    index = 0;
-                }
-            }
-        }
-        FastLED.show();
-    }
-
-void spider_room_lighting(CRGB* leds, int numLeds, int flashes = 10, int frequency = 10) {
+void spider_room_lightning(CRGB* leds, int numLeds, int flashes = 10, int frequency = 15) {
     int ledStart, ledLen, dimmer;
-
+    
     // Determine starting location of flash
-    ledStart = random8(numLeds);                
+    ledStart = random8(numLeds - (numLeds / 4));                
     // Determine length of flash (ensuring it does not exceed the strip length)
-    ledLen = random8(numLeds - ledStart);       
+    ledLen = random8((numLeds / 4), (numLeds - ledStart));     
 
     // Iterate through the number of flashes for this lightning strike
     for (int flashCounter = 0; flashCounter < random8(3, flashes); flashCounter++) {
         // Set dimmer value based on whether it's the first flash (leader) or a return stroke
-        if (flashCounter == 0) dimmer = 5;      
-        else dimmer = random8(1, 3);            
+        if (flashCounter == 0) dimmer = 7;      
+        else dimmer = random8(1, 2);            
 
         // Randomly choose either white or purple for each flash
         CRGB flashColor = random(0, 2) == 0 ? CRGB::White : CRGB(80, 0, 255);
@@ -114,42 +65,46 @@ void spider_room_lighting(CRGB* leds, int numLeds, int flashes = 10, int frequen
             leds[i].nscale8(255 / dimmer); // Adjust brightness of the flash
         }
         FastLED.show();                         
-        delay(random8(5, 20));                  
+        delay(random8(5, 60));                  
 
         // Clear the section of LEDs to create a blinking effect
         fill_solid(leds + ledStart, ledLen, CRGB::Black);
         FastLED.show();
 
         // Delay between the leader and return strokes
-        if (flashCounter == 0) delay(150);      
+        if (flashCounter == 0) delay(random16(100,350));      
         delay(50 + random8(100));               
     }
 
     // Delay between lightning strikes
-    delay(random8(frequency) * 100);            
+    delay(random8(3, frequency) * 100);            
 }
 
-
-
-
     void fairy_room(CRGB* leds) {
-        static uint8_t base_index = 0;
-        base_index++;
-        for (int i = 0; i < NUM_LEDS; i++) {
-            uint8_t colorIndex = (base_index + i * (255 / NUM_LEDS)) % 255;
-            leds[i] = ColorFromPalette(fairy_palette, colorIndex, 255, LINEARBLEND);
-        }
-        FastLED.show();
+            static uint8_t base_index = 0;
+            base_index++;
+            for (int i = 0; i < NUM_LEDS; i++) {
+                uint8_t colorIndex = (base_index + i * (255 / NUM_LEDS)) % 255;
+                leds[i] = ColorFromPalette(fairy_palette, colorIndex, 255, LINEARBLEND);
+            }
     }
 
     void scream_room(CRGB* leds) {
-        static uint8_t base_index = 0;
-        base_index++;
-        for (int i = 0; i < NUM_LEDS; i++) {
-            uint8_t colorIndex = (base_index + i * (255 / NUM_LEDS)) % 255;
-            leds[i] = ColorFromPalette(scream_room_palette, colorIndex, 255, LINEARBLEND);
-        }
-        FastLED.show();
+            static uint8_t base_index = 0;
+            base_index++;
+            for (int i = 0; i < NUM_LEDS; i++) {
+                uint8_t colorIndex = (base_index + i * (255 / NUM_LEDS)) % 255;
+                leds[i] = ColorFromPalette(scream_room_palette, colorIndex, 255, LINEARBLEND);
+            }
+        
+    }
+
+    void default_room(CRGB* leds) {
+        fill_solid(leds, NUM_LEDS, CRGB::Green);
+    }
+
+    void glow_white(CRGB* leds, CRGB colour) {
+        fill_solid(leds, NUM_LEDS, colour);
     }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -7,56 +7,78 @@ static const int UP = 0x18;
 static const int DOWN = 0x52;
 static const int LEFT = 0x8;
 static const int RIGHT = 0x5A;
+
 static const int ONE = 0x45;
 static const int TWO = 0x46;
 static const int THREE = 0x47;
 static const int FOUR = 0x44;
+static const int FIVE = 0x40;
+static const int SIX = 0x43;
+static const int SEVEN = 0x7;
+static const int EIGHT = 0x15;
+static const int NINE = 0x9;
+static const int ZERO = 0x19;
+static const int STAR = 0xFF;
+static const int HASH = 0xD;
+
 static const int OKAY = 0x1C;
 
 // Variable to store the active configuration
 int activeConfig = 0;
 
-void setup() {
+bool switch_strips = false;
+
+void setup()
+{
     Serial.begin(115200);
     IrReceiver.begin(4);
 }
 
-void loop() {
-    if (IrReceiver.decode()) {
-        if (!(IrReceiver.decodedIRData.flags & IRDATA_FLAGS_IS_REPEAT)) {
-            switch (IrReceiver.decodedIRData.command) {
+void loop()
+{
+    if (IrReceiver.decode())
+    {
+        if (!(IrReceiver.decodedIRData.flags & IRDATA_FLAGS_IS_REPEAT))
+        {
+            lighting.clear_strip(lighting.leds1);
+            lighting.clear_strip(lighting.leds2);
+            switch (IrReceiver.decodedIRData.command)
+            {
             case DOWN:
+                lighting.set_brightness(-40);
+                break;
             case LEFT:
                 lighting.set_brightness(-20);
                 break;
             case UP:
+                lighting.set_brightness(40);
+                break;
             case RIGHT:
                 lighting.set_brightness(20);
                 break;
             case ONE:
-                lighting.clear_strip(lighting.leds1);  // Clear leds1
-                lighting.clear_strip(lighting.leds2);  // Clear leds2
-                activeConfig = 1; // Set to Configuration 1: Fairy room on leds1, scream room on leds2
+                switch_strips = !switch_strips;
+                activeConfig = 1;
                 break;
             case TWO:
-                lighting.clear_strip(lighting.leds1);  // Clear leds1
-                lighting.clear_strip(lighting.leds2);  // Clear leds2
-                activeConfig = 2; // Set to Configuration 2: Petting zoo room on leds1, fairy room on leds2
+                switch_strips = !switch_strips;
+                activeConfig = 2;
                 break;
             case THREE:
-                lighting.clear_strip(lighting.leds1);  // Clear leds1
-                lighting.clear_strip(lighting.leds2);  // Clear leds2
-                activeConfig = 3; // Set to Configuration 3: Spider room on leds1, spider room lightning on leds2
+                activeConfig = 3;
                 break;
             case FOUR:
-                lighting.clear_strip(lighting.leds1);  // Clear leds1
-                lighting.clear_strip(lighting.leds2);  // Clear leds2
-                activeConfig = 4; // Set to Configuration 4: Scream room on leds1, petting zoo room on leds2
+                activeConfig = 4;
                 break;
+            case FIVE:
+                activeConfig = 5;
+                break;
+            case SIX:
+                activeConfig = 6;
             case OKAY:
-                lighting.clear_strip(lighting.leds1);  // Clear leds1
-                lighting.clear_strip(lighting.leds2);  // Clear leds2
-                activeConfig = 0; // No active configuration after clearing
+                lighting.clear_strip(lighting.leds1);
+                lighting.clear_strip(lighting.leds2);
+                activeConfig = 7;
                 break;
             default:
                 break;
@@ -65,23 +87,75 @@ void loop() {
         IrReceiver.resume();
     }
 
-    // Apply the currently active configuration
-    switch (activeConfig) {
+    switch (activeConfig)
+    {
     case 1:
-        lighting.fairy_room(lighting.leds1);
-        lighting.scream_room(lighting.leds2);
+        if (switch_strips)
+        {
+            EVERY_N_MILLIS(100)
+            {
+                lighting.fairy_room(lighting.leds1);
+            }
+            EVERY_N_MILLIS(100)
+            {
+                lighting.scream_room(lighting.leds2);
+            }
+        }
+        else
+        {
+            EVERY_N_MILLIS(100)
+            {
+                lighting.fairy_room(lighting.leds2);
+            }
+            EVERY_N_MILLIS(100)
+            {
+                lighting.scream_room(lighting.leds1);
+            }
+        }
         break;
     case 2:
-        lighting.scream_room(lighting.leds1);
-        lighting.spider_room_lightning(lighting.leds2, 216);
+        if (switch_strips)
+        {
+            EVERY_N_MILLIS(100)
+            {
+                lighting.scream_room(lighting.leds1);
+            }
+            lighting.spider_room_lightning(lighting.leds2, 237);
+        }
+        else
+        {
+            EVERY_N_MILLIS(100)
+            {
+                lighting.scream_room(lighting.leds2);
+            }
+            lighting.spider_room_lightning(lighting.leds1, 237);
+        }
+        Serial.println(switch_strips);
         break;
     case 3:
-        lighting.spider_room_lightning(lighting.leds1, 216);
-        lighting.spider_room_lightning(lighting.leds2, 216);
+        lighting.spider_room_lightning(lighting.leds1, 237);
+        lighting.spider_room_lightning(lighting.leds2, 237);
         break;
+    case 4:
+        lighting.glow_white(lighting.leds1, CRGB::White);
+        lighting.glow_white(lighting.leds2, CRGB::White);
+        break;
+    case 5:
+        EVERY_N_MILLIS(100)
+        {
+            lighting.fairy_room(lighting.leds1);
+        }
+        EVERY_N_MILLIS(100)
+        {
+            lighting.fairy_room(lighting.leds2);
+        }
+        break;
+    case 6:
+    case 7:
     default:
-        lighting.clear_strip(lighting.leds1);
-        lighting.clear_strip(lighting.leds2);
+        lighting.default_room(lighting.leds1);
+        lighting.default_room(lighting.leds2);
         break;
     }
+    FastLED.show();
 }
